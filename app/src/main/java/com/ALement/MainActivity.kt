7f7779ac8ae1b4cb956268d5f7c4939242ee1548
kaldwar <jalkaru@laposte.net>
2019-08-30 10:32:58 +0200

    Initial commit

---
+++ app/src/main/java/com/ALement/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.ALement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Chinois", "Burger King", "Kebab", "Pizza", "Sandwich", "Mcdonald's", "KFC")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        choiceBtn.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodText.text = foodList[randomFood]
+
+        }
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodText.text.toString()
+            foodList.add(newFood)
+            addFoodText.text.clear()
+            println(foodList)
+
+        }
+    }
+}
